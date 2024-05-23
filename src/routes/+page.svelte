fdd975fc48d703dc5365ba9a793186555d400d85
jeremt <taboada.jeremie@gmail.com>
2024-05-23 16:02:12 +0200

    feat(landing): initial landing html

---
+++ src/routes/+page.svelte
@@ -0,0 +1,114 @@
+<script lang="ts">
+</script>
+
+<header>
+    <div id="logo">les3dev</div>
+    <nav>
+        <a href="#intro">Intro</a>
+        <a href="#projects">Projets</a>
+        <a href="#faq">FAQ</a>
+        <a href="#contact">Contact</a>
+    </nav>
+</header>
+
+<section>
+    <h1>Vous avez les idées,<br />On les fait marcher.</h1>
+    <a role="button" href="mailto:contact@les3.dev">Demander un devis</a>
+</section>
+<section>
+    <h2>Nos projets</h2>
+    <div class="subtitle">Échantillion non-exaustif de notre travail de ces dernières années</div>
+    <article>
+        <a href="https://demo.voltask.tech" target="_blank">
+            <img src="/images/voltask.png" alt="" />
+            <h3>Voltask</h3>
+            <p>Outil no-code collaboratif dans le navigateur pour créer des automatisations avec des scénarios visuels.</p>
+        </a>
+    </article>
+    <article>
+        <a href="https://codepassport.dev" target="_blank">
+            <img src="/images/codepassport.png" alt="" />
+            <h3>Code Passport</h3>
+            <p>Site web interactif destiné à l'apprentissage du code (JS, HTML & CSS).</p>
+        </a>
+    </article>
+    <article>
+        <a href="https://chat.voltask.tech" target="_blank">
+            <img src="/images/voltask-ai.png" alt="" />
+            <h3>Voltask AI</h3>
+            <p>Chatbot IA pour lancer des automatisations Voltask.</p>
+        </a>
+    </article>
+    <article>
+        <a href="https://editor.voltapp.tech" target="_blank">
+            <img src="/images/voltapp.png" alt="" />
+            <h3>Voltapp</h3>
+            <p>Voltapp est un outil no-code permettant de créer des applications web grâce à la programmation visuelle.</p>
+        </a>
+    </article>
+    <article>
+        <a href="https://creator.celestory.io" target="_blank">
+            <img src="/images/celestory.png" alt="" />
+            <h3>Celestory</h3>
+            <p>Outil pour créer des serious games, formations et visual novels avec un système de choix et de variables en no-code.</p>
+        </a>
+    </article>
+    <article>
+        <a href="https://ttmc-ui.vercel.app" target="_blank">
+            <img src="/images/ttmc-for-dev.png" alt="" />
+            <h3>TTMC for dev</h3>
+            <p>Inspiré par le jeu "Tu te mets combien ?" pour aider les apprenti.e.s développeur.ses à améliorer leur culture tech.</p>
+        </a>
+    </article>
+    <article>
+        <a href="https://photomapper.io" target="_blank">
+            <img src="/images/photomapper.png" alt="" />
+            <h3>Photomapper</h3>
+            <p>Application mobile de partage de photos géolocalisées partout dans le monde.</p>
+        </a>
+    </article>
+    <article>
+        <a href="https://apps.apple.com/us/app/crafter-ar-build-battle/id1389840361" target="_blank">
+            <img src="/images/crafter-ar.png" alt="" />
+            <h3>Crafter AR</h3>
+            <p>Jeu mobile de pictionary en réalité augmenté et en temps réel.</p>
+        </a>
+    </article>
+</section>
+<section id="team">
+    <h2>L'équipe</h2>
+    <article>
+        <img src="/images/jeremie.png" alt="" />
+        <div class="name">Jeremie</div>
+        <a href="https://www.linkedin.com/in/jeremie-taboada-16495959/" target="_blank">Linked In</a>
+        <a href="https://github.com/jeremt" target="_blank">Github</a>
+    </article>
+    <article>
+        <img src="/images/jonathan.png" alt="" />
+        <div class="name">Jonathan</div>
+        <a href="https://www.linkedin.com/in/jonathanpicques/" target="_blank">Linked In</a>
+        <a href="https://github.com/jonathanpicques" target="_blank">Github</a>
+    </article>
+    <article>
+        <img src="/images/vincent.png" alt="" />
+        <div class="name">Vincent</div>
+        <a href="https://www.linkedin.com/in/vincentneel/" target="_blank">Linked In</a>
+        <a href="https://github.com/pikanezi" target="_blank">Github</a>
+    </article>
+</section>
+<section id="faq">
+    <h2>Foire aux questions</h2>
+    <details>
+        <summary>Pourquoi 3 développeurs ?</summary>
+        Nous avons l’habitude de travailler ensemble depuis plusieurs années. Grâce à cette experience et nos différentes affinités et domaines d’expertise nous
+        pouvons nous répartir efficacement pour répondre à vos demandes les plus folles.
+    </details>
+    <details>
+        <summary>Comment faire évoluer mon projet ?</summary>
+        Vous avez les clés
+    </details>
+</section>
+<section id="contact">
+    <h2>Nous contacter</h2>
+    <a role="button" href="mailto:contact@les3.dev">Demander un devis</a>
+</section>
