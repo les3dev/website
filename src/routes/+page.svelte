5620b86b4c68d046d1338e2d82c2dbc9163ec778
JonathanPicques <jonathan.picques@gmail.com>
2024-06-05 11:16:46 +0200

    fix: scroll x

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -395,8 +395,10 @@
 
     #sky {
         position: absolute;
-        width: 100vw;
-        height: 100vh;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
         overflow: hidden;
     }
 
