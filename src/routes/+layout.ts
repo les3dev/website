3d03ae552c33d529fb7fd2d27f2d54150b6b811c
jeremt <taboada.jeremie@gmail.com>
2024-06-25 16:15:55 +0200

    feat(posthog): avoid duplication of pageview/pageleave events

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,7 +4,7 @@ import {PUBLIC_POSTHOG_API_KEY} from '$env/static/public';
 
 export const load = async () => {
     if (browser && !dev) {
-        posthog.init(PUBLIC_POSTHOG_API_KEY, {api_host: '/ingest', ui_host: 'https://eu.posthog.com'});
+        posthog.init(PUBLIC_POSTHOG_API_KEY, {api_host: '/ingest', ui_host: 'https://eu.posthog.com', capture_pageview: false, capture_pageleave: false});
     }
     return {};
 };
